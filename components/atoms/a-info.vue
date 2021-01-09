2de82e0763404132dcc5d5a5d0b57f2a59f25008
sakthiraj93 <sakthiraj93@gmail.com>
2021-01-09 17:15:35 +0530

    Feature(New Header): New Header Layout Design

---
+++ components/atoms/a-info.vue
@@ -0,0 +1,11 @@
+<template>
+    <a>i</a>
+</template>
+
+<script>
+import { SfIcon } from '@storefront-ui/vue';
+
+export default {
+  components: { SfIcon }
+};
+</script>
--- components/atoms/a-topbar.vue
+++ components/atoms/a-topbar.vue
@@ -1,97 +1,81 @@
 <template>
   <div class="top-bar d-flex justify-between">
+    <p class="text_upper">We are specialized in DIY and gardening tools</p>
     <ul class="d-flex" id="md_top_comfort_links">
       <li>
-        <a href="/pages/our-promise" class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc"
-          >Our Promise</a
-        >
+        <a
+          href="/pages/our-promise"
+          class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc">
+          Our Promise
+          </a>
       </li>
       <li>
-        <a href="/pages/delivery" class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc"
-          >Free Delivery</a
-        >
+        <a
+          href="/pages/delivery"
+          class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc">
+          Free Delivery
+          </a>
       </li>
       <li>
-        <a href="/pages/expert-advice" class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc"
-          >Expert Advice</a
-        >
+        <a
+          href="/pages/expert-advice"
+          class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc">
+          Expert Advice
+          </a>
       </li>
       <li class="last">
-        <a href="/pages/returns" class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc"
-          >30 Day Returns</a
-        >
+        <a href="/pages/returns" class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc">
+        30 Day Returns
+        </a>
       </li>
-    </ul>
-    <ul class="d-flex creadential-menu">
       <li>
-            <SfButton class="sf-button--text" @click.native="goToAccount">
-              {{ $t("SIGN IN") }}
-            </SfButton> 
-              Or
-            <SfButton class="sf-button--text" @click.native="switchElem('register')">
-              {{ $t("REGISTER") }}
-            </SfButton>
-        </li>
-         <li>
-            <a href="/pages/capitools-service-and-customer-care">
-             Help
-            </a>
-        </li>
-        <li>
-          <a>
-             <SfIcon
-              icon="heart_fill"
-              color="white"
-              size="16px"
-            />
-          </a>
-        </li>
-        <li>
-          <a>
-             <SfIcon
-              icon="more"
-              color="white"
-              size="16px"
-            />
-          </a>
-        </li>
+        <a> Wishlist </a>
+      </li>
+      <li>
+        <a href="/pages/capitools-service-and-customer-care"> Help </a>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-import { SfIcon, SfButton } from '@storefront-ui/vue';
-import { mapGetters, mapActions } from 'vuex';
-import { ModalList } from 'theme/store/ui/modals'
-export default { 
+import { SfIcon, SfButton } from "@storefront-ui/vue";
+import { mapGetters, mapActions } from "vuex";
+import { ModalList } from "theme/store/ui/modals";
+export default {
   components: { SfIcon, SfButton },
   computed: {
-    ...mapGetters('user', ['isLoggedIn'])
+    ...mapGetters("user", ["isLoggedIn"]),
   },
   methods: {
-    ...mapActions('ui', {
-      openModal: 'openModal'
+    ...mapActions("ui", {
+      openModal: "openModal",
     }),
-    goToAccount () {
-      this.openModal({ name: ModalList.Auth, payload: 'login' })
+    goToAccount() {
+      this.openModal({ name: ModalList.Auth, payload: "login" });
     },
-    switchElem (to) { 
-      this.openModal({ name: ModalList.Auth, payload: to })
+    switchElem(to) {
+      this.openModal({ name: ModalList.Auth, payload: to });
     },
-  }
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .top-bar {
-  background-color: var(--_c-green-primary);
-   @media screen and (max-width: 820px) {
-      flex-direction: column;
-   }
-  ul {
-    padding: 3px 30px;
+  background-color: var(--_c-gray-accent);
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+  p {
     margin: 0;
-    list-style: none; 
+    font-size: 13px;
+    padding: 5px 30px;
+  }
+  ul {
+    padding: 5px 30px;
+    margin: 0;
+    list-style: none;
     @media screen and (max-width: 820px) {
       padding: 5px 15px;
     }
@@ -102,25 +86,26 @@ export default {
     li {
       margin: 0 10px;
       color: var(--c-white);
-      text-transform: uppercase;
-      font-size: 12px;
+      font-size: 13px;
       @media screen and (max-width: 480px) {
         margin: 0 6px;
         font-size: 11px;
       }
-      a, button {
-        color: var(--c-white);
-        font-size: 12px;
-        text-transform: uppercase;
+      a,
+      button {
+        color: var(--_c-dark-secondary);
+        font-size: 13px;
         text-decoration: none;
+        text-transform: none;
         @media screen and (max-width: 480px) {
           font-size: 11px;
         }
         &:hover {
-          text-decoration: underline;
+          text-decoration: none;
+          color: var(--_c-green-primary);
         }
       }
-      button{
+      button {
         margin: 0 5px;
       }
     }
