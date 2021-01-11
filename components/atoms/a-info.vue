70d2c698ae9000e49df579069e9da91f4726c437
sakthiraj93 <sakthiraj93@gmail.com>
2021-01-11 18:40:43 +0530

    Feature(Info Menu) - Info Menu Design for Mobile & Tablet view

--- components/atoms/a-info.vue
+++ components/atoms/a-info.vue
@@ -1,11 +1,67 @@
 <template>
-    <a>i</a>
+  <SfSelect
+    v-model="selectedValue"
+    :class="customClass"
+    :label="label"
+    :required="required"
+    :valid="valid"
+    :disabled="disabled"
+    :error-message="errorMessage"
+    :placeholder="placeholder"
+    @input="alert(selectedValue)"
+    style="max-width: 30rem; margin: 10px;"
+  >
+  <li>
+        <a
+          href="/pages/our-promise"
+          class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc">
+          Our Promise
+          </a>
+      </li>
+      <li>
+        <a
+          href="/pages/delivery"
+          class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc">
+          Free Delivery
+          </a>
+      </li>
+      <li>
+        <a
+          href="/pages/expert-advice"
+          class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc">
+          Expert Advice
+          </a>
+      </li>
+      <li class="last">
+        <a href="/pages/returns" class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc">
+        30 Day Returns
+        </a>
+      </li>
+      <li>
+        <a> Wishlist </a>
+      </li>
+      <li>
+        <a href="/pages/capitools-service-and-customer-care"> Help </a>
+      </li> 
+  </SfSelect>
 </template>
-
 <script>
-import { SfIcon } from '@storefront-ui/vue';
-
+import { SfSelect } from "@storefront-ui/vue";
+import { SfProductOption } from "@storefront-ui/vue";
 export default {
-  components: { SfIcon }
+  components: {
+    SfSelect,
+    SfProductOption,
+  },
+  data() {
+    return { 
+      label: "i",
+      required: false,
+      valid: true,
+      disabled: false,
+      errorMessage: "Color",
+      placeholder: "select color",
+    };
+  },
 };
 </script>
