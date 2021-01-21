0489991dd9c91686d7f602f7dacc780c69043738
maheshwaran-1343 <maheswaran@superbotics.in>
2021-01-21 18:36:46 +0530

    adjust(warning):warning fixed in info section in mobile & tablet view

--- components/atoms/a-info.vue
+++ components/atoms/a-info.vue
@@ -1,44 +1,20 @@
 <template>
-  <SfSelect
-    v-model="selectedValue" 
-    :label="label"
-    :required="required"
-    :valid="valid"
-    :disabled="disabled"
-    :error-message="errorMessage"
-    :placeholder="placeholder"
-    @input="alert(selectedValue)"
-    style="max-width: 30rem; margin: 10px;"
-  >
-  <li>
-        <a
-          href="/pages/our-promise"
-          class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc">
-          Our Promise
-          </a>
-      </li>
-      <li>
-        <a
-          href="/pages/delivery"
-          class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc">
-          Free Delivery
-          </a>
-      </li>
-      <li>
-        <a
-          href="/pages/expert-advice"
-          class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc">
-          Expert Advice
-          </a>
-      </li>
-      <li class="last">
-        <a href="/pages/returns" class="md-opjjpmhoiojifppkkcdabiobhakljdgm_doc">
-        30 Day Returns
-        </a>
-      </li>
-      <li>
-        <a href="/pages/capitools-service-and-customer-care"> Help </a>
-      </li> 
+  <SfSelect label="i" valid value=""  style="max-width: 30rem; margin: 10px;">
+    <SfSelectOption value=""> 
+      <li> <a href="/pages/our-promise">Our Promise</a> </li>
+    </SfSelectOption>
+    <SfSelectOption value=""> 
+      <li>  <a href="/pages/delivery"> Free Delivery</a> </li>
+    </SfSelectOption>
+    <SfSelectOption value=""> 
+      <li> <a href="/pages/expert-advice">Expert Advice</a> </li>
+    </SfSelectOption>
+    <SfSelectOption value=""> 
+      <li class="last"> <a href="/pages/returns">30 Day Returns</a> </li>
+    </SfSelectOption>
+    <SfSelectOption value=""> 
+      <li> <a href="/pages/capitools-service-and-customer-care">Help</a> </li>
+    </SfSelectOption> 
   </SfSelect>
 </template>
 <script>
@@ -48,16 +24,6 @@ export default {
   components: {
     SfSelect,
     SfProductOption,
-  },
-  data() {
-    return { 
-      label: "i",
-      required: false,
-      valid: true,
-      disabled: false,
-      errorMessage: "Color",
-      placeholder: "select color",
-    };
   },
 };
 </script>
