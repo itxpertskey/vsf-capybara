a8db9c3072596e628049f4d0fdfa40cc3bd5ec44
maheshwaran-1343 <maheswaran@superbotics.in>
2021-01-21 10:57:39 +0530

    adjust(assurance block):set link to assurance block

--- components/atoms/a-assurance-delivery.vue
+++ components/atoms/a-assurance-delivery.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="d-flex align-center">
-        <SfImage
-        src="/assets/icons/delivery-icon.png"
-        class="image_delivery"
-        />
-        <div class="text-center">
-            <h6>Free Delivery </h6>
-            <p>on Order Over £50</p>
-        </div>
+         <SfLink link="/pages/delivery">  
+            <SfImage
+            src="/assets/icons/delivery-icon.png"
+            class="image_delivery"
+            />
+            <div class="text-center">
+                <h6>Free Delivery </h6>
+                <p>on Order Over £50</p>
+            </div>
+        </SfLink>
     </div>
 </template>
 
 <script>
-import { SfImage } from '@storefront-ui/vue';
-export default { components: { SfImage } };
+import { SfImage, SfLink } from '@storefront-ui/vue';
+export default { components: { SfImage, SfLink } };
 </script>
