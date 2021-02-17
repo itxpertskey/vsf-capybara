93faff9c2594711d2623a3c2d519d3e2787362ae
maheshwaran-1343 <maheswaran@superbotics.in>
2021-02-17 11:43:47 +0530

    adjust(subscribe popup):add subscribe popup

--- components/molecules/modals/m-modal-thank-you.vue
+++ components/molecules/modals/m-modal-thank-you.vue
@@ -5,10 +5,12 @@
         <div class="padding">
           <h3 class="text-center text-primary">Your request has been registered successfully. Please find your code below :</h3>
           <div class="coupon-code d-flex align-center">
-              <h1>Welcome5</h1>
-              <span>Click to copy code</span>
+              <h1>Welcome5%</h1>
+               <span id="copy_code_text" @click.stop.prevent="copyVoucherCode">Click to copy code</span>  
           </div>
-            <p>You will receive a voucher code to the entered email address.</p>
+            <p>You will receive a voucher code to the entered email address.
+               <input type="text" value="WELCOME5%" id="vocher_code" style="opacity:0">
+            </p>  
         </div>
     </SfModal>
   </div>
@@ -36,7 +38,14 @@ export default {
   methods: {
     closeModal () {
       this.$emit('close', this.modalData.name)
-    }
+    },
+    copyVoucherCode(){
+      document.getElementById("copy_code_text").textContent="Copied!";
+      var copyText = document.getElementById("vocher_code");
+      copyText.select();
+      copyText.setSelectionRange(0, 99999);
+      document.execCommand("copy");  
+    }  
   }
 };
 </script>
